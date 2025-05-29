9c75316f7abfa383a9a8486b5133b0da191310b7
MIN0225 <cro77@naver.com>
2025-05-29 09:32:59 +0900

    test: 맛집 성향 입력 도시, 지역 테스트 추가 / RestaurantRecommend 테스트

--- Frontend/src/tests/RestaurantInput.test.js
+++ Frontend/src/tests/RestaurantInput.test.js
@@ -19,6 +19,14 @@ describe("맛집 성향 테스트", () => {
     fireEvent.click(screen.getByLabelText("한식"));
     fireEvent.click(screen.getByLabelText("데이트"));
 
+    // 여행 도시, 도시 내 지역 입력
+    fireEvent.change(screen.getByPlaceholderText("ex) 서울"), {
+      target: { value: "서울" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("ex) 해운대"), {
+      target: { value: "여의도"},
+    })
+
     // 버튼 클릭
     fireEvent.click(screen.getByText("분석 시작하기"));
 
@@ -28,6 +36,8 @@ describe("맛집 성향 테스트", () => {
       companions: ["친구와"],
       foodTypes: ["한식"],
       atmospheres: ["데이트"],
+      city: "서울",
+      region: "여의도",
     });
   });
 });
