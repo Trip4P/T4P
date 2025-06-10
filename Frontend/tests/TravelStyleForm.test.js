0f61c19ca2008dd93ea0a99ef69cccc2af120a46
MIN0225 <cro77@naver.com>
2025-06-10 11:29:48 +0900

    test: 사용자 여행 성향 입력 페이지 테스트

--- Frontend/tests/TravelStyleForm.test.js
+++ Frontend/tests/TravelStyleForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, within } from "@testing-library/react";
-import TravelStyleForm from "../src/pages/TravelStyleForm";
+import TravelStyleForm from "../src/pages/travel/TravelStyleForm";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
@@ -9,7 +9,7 @@ describe("TravelStyleForm", () => {
     localStorage.clear(); // 초기화
   });
 
-  test("사용자 입력 정보가 localStorage에 저장된다", async () => {
+  test("사용자 여행 성향 입력 테스트", async () => {
     render(
       <MemoryRouter>
         <TravelStyleForm />
@@ -22,24 +22,27 @@ describe("TravelStyleForm", () => {
     fireEvent.change(screen.getByPlaceholderText("ex) 서울역"), {
       target: { value: "서울역" },
     });
+    
+
     fireEvent.change(screen.getByPlaceholderText("ex) 잠실"), {
       target: { value: "부산역" },
     });
+    
 
     // 여행 시작일 입력
     await user.click(screen.getByPlaceholderText("시작일 선택"));
     const startPopup = screen.getByRole("dialog");
     const startCalendar = within(startPopup);
-    await user.click(startCalendar.getByText("1")); // 1일 클릭
+    await user.click(startCalendar.getAllByText("1")[0]); // 1일 클릭
 
     // // 여행 종료일 입력
     await user.click(screen.getByPlaceholderText("종료일 선택"));
     const endPopup = screen.getByRole("dialog");
     const endCalendar = within(endPopup);
-    await user.click(endCalendar.getByText("3")); // 3일 클릭
+    await user.click(endCalendar.getAllByText("3")[0]);
 
     // 감정 체크 (예: "설레는"이라는 감정 체크박스가 있다고 가정)
-    fireEvent.click(screen.getByLabelText("설레는"));
+    fireEvent.click(screen.getByLabelText("💗 설레는"));
 
     // 인원 수 입력
     fireEvent.change(screen.getByLabelText("인원수"), {
@@ -52,11 +55,12 @@ describe("TravelStyleForm", () => {
     // 로컬스토리지에 저장된 데이터 확인
     const stored = JSON.parse(localStorage.getItem("travelStyle"));
     // console.log("stored: ", stored);
-    expect(stored.departure).toBe("서울역");
-    expect(stored.destination).toBe("부산역");
+
+    expect(stored.startCity).toBe("서울역");
+    expect(stored.endCity).toBe("부산역");
     expect(stored.startDate).toBeDefined();
     expect(stored.endDate).toBeDefined();
     expect(stored.peopleCount).toBe(2);
-    expect(stored.emotion).toContain("설레는");
+    expect(stored.emotions).toContain("💗 설레는");
   });
 });
